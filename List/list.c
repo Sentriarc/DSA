34e4753553380129995eacee9d93466a38d7b019
Sentriarc <tran.christopherd1@gmail.com>
2024-10-15 21:49:41 -0500

    added potential fault comment for later use

--- List/list.c
+++ List/list.c
@@ -4,7 +4,7 @@ int main() {
     // 1.) Sum of Elements
 
     // Array has 5 current numbers, with one extra slot
-    int numbers[6] = {1, 2, 3, 4, 5};
+    int numbers[] = {1, 2, 3, 4, 5, 0};
 
 
     // Prompting for user input for sixth value in array
@@ -15,7 +15,7 @@ int main() {
 
 
     // Appending user's choice into numbers array
-    numbers[6] = answer;
+    numbers[6] = answer; // <<<< there is something wrong here (i think)
     for (int i = 0; i < 6; i++) {
         printf("%d\n", numbers[i]);
     }
