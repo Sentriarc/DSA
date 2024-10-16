8d0e50d65a775592c02b9746f97b82ecd5c97800
Sentriarc <tran.christopherd1@gmail.com>
2024-10-15 21:38:45 -0500

    added array

---
+++ List/list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // 1.) Sum of Elements
+
+    // Array has 5 current numbers, with one extra slot
+    int numbers[6] = {1, 2, 3, 4, 5};
+
+
+    // Prompting for user input for sixth value in array
+    int answer;
+    printf("Type a number: \n");
+    scanf("%d", &answer);
+    // printf("The number you typed: %d", answer);
+
+
+    // Appending user's choice into numbers array
+
+
+
+    // Adding up all numbers in array
+
+
+
+    // Printing sum
+
+
+
+    return 0;
+}
