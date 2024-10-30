145272b66dd9a6398e76328f6694adf8af81298b
Sentriarc <tran.christopherd1@gmail.com>
2024-10-30 13:46:35 -0500

    completed file with AI, will now practice

--- Linked Lists/linked-list.c
+++ Linked Lists/linked-list.c
@@ -1,10 +1,37 @@
 #include <stdio.h>
 
-struct Node {
-    int num1 = 4;
-    Node* 
-}
+
+// THE ENTIRETY OF THE CODE BELOW WAS WRITTEN BY CHATGPT
+struct LinkedList {
+    int num;
+    struct LinkedList *next; // Pointer to the next node
+    struct LinkedList *prev; // Pointer to the previous node
+}; // End with a semicolon
 
 int main() {
+    // Define nodes statically
+    struct LinkedList node1, node2, node3;
 
+    // Initialize the data
+    node1.num = 1;
+    node2.num = 2;
+    node3.num = 3;
+
+    // Link the nodes
+    node1.next = &node2;
+    node2.next = &node3;
+    node3.next = NULL;    // Last node points to NULL
+
+    node1.prev = NULL;    // First node prev is NULL
+    node2.prev = &node1;
+    node3.prev = &node2;
+
+    // Print the linked list
+    struct LinkedList* ptr = &node1;
+    while (ptr != NULL) {
+        printf("%d ", ptr->num);
+        ptr = ptr->next;
+    }
+
+    return 0; 
 }
