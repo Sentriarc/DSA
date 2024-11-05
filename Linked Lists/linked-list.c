ea1e6c3362cff405e73265041fb62168366ce3be
Sentriarc <tran.christopherd1@gmail.com>
2024-11-04 20:22:31 -0600

    reduced errors to 1, but void function is not being used properly

--- Linked Lists/linked-list.c
+++ Linked Lists/linked-list.c
@@ -2,9 +2,7 @@
 
 // Welcome to our facility, where we stack boxes for you! You also have the option to use some of ours!
 
-void Stacking(int *box1, int *box2) {
-    (*box1) + (*box2);
-}
+void Stacking(int *box1, int *box2);
 
 int main() {
     int userChoice1;
@@ -16,8 +14,14 @@ int main() {
     printf("How many boxes will you need from our facility today?\n");
     scanf("%d", &userChoice2);
 
-    int allBoxes = Stacking(int userChoice1, int userChoice2);
+    int allBoxes = Stacking(&userChoice1, &userChoice2);
+    // int allBoxes = Stacking(&userChoice1, &userChoice2);
     printf("Here's how many boxes you will be stacking: %d", allBoxes);
 
+
     return 0;
+}
+
+void Stacking(int *box1, int *box2) {
+    (*box1) + (*box2);
 }
