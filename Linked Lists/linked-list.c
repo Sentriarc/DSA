2abadcbbb188f5d4fe2f19fcc95f064db40935cb
Sentriarc <tran.christopherd1@gmail.com>
2024-11-04 20:37:18 -0600

    changed void functions to int

--- Linked Lists/linked-list.c
+++ Linked Lists/linked-list.c
@@ -2,26 +2,26 @@
 
 // Welcome to our facility, where we stack boxes for you! You also have the option to use some of ours!
 
-void Stacking(int *box1, int *box2);
+int Stacking(int *box1, int *box2);
 
 int main() {
     int userChoice1;
     int userChoice2;
 
-    printf("How many boxes will you be bringing in today?\n");
+    printf("How many boxes will you be bringing in?\n");
     scanf("%d", &userChoice1);
 
-    printf("How many boxes will you need from our facility today?\n");
+    printf("How many boxes will you need from our facility?\n");
     scanf("%d", &userChoice2);
 
     int allBoxes = Stacking(&userChoice1, &userChoice2);
     // int allBoxes = Stacking(&userChoice1, &userChoice2);
-    printf("Here's how many boxes you will be stacking: %d", allBoxes);
+    printf("Here's how many boxes you will be stacking: %d\n", allBoxes);
 
 
     return 0;
 }
 
-void Stacking(int *box1, int *box2) {
-    (*box1) + (*box2);
+int Stacking(int *box1, int *box2) {
+    return (*box1) + (*box2);
 }
