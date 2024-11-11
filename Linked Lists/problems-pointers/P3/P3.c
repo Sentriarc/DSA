a5815f511968c746f90406babc1d102b33d41c36
Sentriarc <tran.christopherd1@gmail.com>
2024-11-11 15:43:50 -0600

    added input request

--- Linked Lists/problems-pointers/P3/P3.c
+++ Linked Lists/problems-pointers/P3/P3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void stringChanger(int* arr, int size, int targetValue) {
     for (int i = 0; i < size; i++) {
@@ -7,5 +8,14 @@ void stringChanger(int* arr, int size, int targetValue) {
 }
 
 int main() {
-    
+    int targetValue;
+    char string[100];
+
+    // Asking for user input
+    printf("Say something! (Preferably a string): ");
+    scanf("%s\n", string);
+    // printf("%s", toupper(char string));
+    for (int i = 0; i < sizeof(string), i++;) { // does not work
+        printf("this works");
+    };
 }
