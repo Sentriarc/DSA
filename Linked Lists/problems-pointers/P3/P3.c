72b28bd2eb83acc7174e33bd4d41b816620558ad
Sentriarc <tran.christopherd1@gmail.com>
2024-11-11 10:36:27 -0600

    added basic void function for array

--- Linked Lists/problems-pointers/P3/P3.c
+++ Linked Lists/problems-pointers/P3/P3.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void stringChanger(int* arr, int size, int targetValue) {
+    for (int i = 0; i < size; i++) {
+        *(arr + i) = targetValue;
+    }
+}
+
 int main() {
     
 }
